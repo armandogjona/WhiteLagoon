f3f63a170372cab5c3a1e7b0dba60053aed2240f
Armando Gjona <agjona23@beder.edu.al>
2024-10-18 18:54:57 +0200

    create villa UI

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // _db retrieves all the Villa
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
