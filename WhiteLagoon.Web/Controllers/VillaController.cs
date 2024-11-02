0ec32ca27f38ca8499aafee19e8fe5e68a8506f2
Armando Gjona <agjona23@beder.edu.al>
2024-11-02 17:49:03 +0100

    villa model done

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -21,6 +21,10 @@ namespace WhiteLagoon.Web.Controllers
         [HttpPost] //sends data to a server to create or update a resource
         public IActionResult Create(Villa obj)
         {
+            if(obj.Name==obj.Description)
+            {
+                ModelState.AddModelError("","The description cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Villas.Add(obj);
