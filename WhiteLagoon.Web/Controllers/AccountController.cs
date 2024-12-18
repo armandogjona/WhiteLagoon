293158ffb923c124730ae3e1a7ffb1b5766af64b
armandogjona <agjona23@beder.edu.al>
2024-12-18 22:28:30 +0100

    login and register VM

---
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Repository;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager; //used to manage a user
+        private readonly SignInManager<ApplicationUser> _signInManager; //used to sign in a user
+        private readonly RoleManager<IdentityRole> _roleManager; //used to manage roles
+
+        public AccountController(IUnitOfWork unitOfWork,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
