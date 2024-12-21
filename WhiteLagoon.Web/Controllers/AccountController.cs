539900587b308e7d8f2e8f287de6d8d469c07ce1
armandogjona <agjona23@beder.edu.al>
2024-12-21 15:19:03 +0100

    created register and login views

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Repository;
+using WhiteLagoon.Web.ViewModels;
 
 namespace WhiteLagoon.Web.Controllers
 {
@@ -23,13 +26,34 @@ namespace WhiteLagoon.Web.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl = null)
         {
-            return View();
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new ()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
         }
         public IActionResult Register()
         {
-            return View();
+            if(!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).Wait();
+            }
+
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                })
+            };
+            return View(registerVM);
         }
     }
 }
